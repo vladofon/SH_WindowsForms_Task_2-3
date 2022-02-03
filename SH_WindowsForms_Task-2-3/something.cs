84737277cc9cd0440052ccd2a9f49d2d0dca7a84
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-02-03 21:44:33 +0200

    Add project files.

---
+++ SH_WindowsForms_Task-2-3/something.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SH_WindowsForms_Task_2_3
+{
+    public partial class something : Form
+    {
+        public something()
+        {
+            InitializeComponent();
+        }
+    }
+}
